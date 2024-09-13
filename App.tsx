989c987758bc796231bad10cb8fdba81d6681a22
jebsdev <>
2024-09-13 07:54:48 -0300

    kkkkkk
    kkkkkkk

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {View, Text, StyleSheet } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default function app () {
+  return(
+    <View style={styles.conteiner}> 
+      <View style = {styles.box1}>
+        <Text>Box 1</Text>
+      </View>
+      <View>
+        <Text>Box 2</Text>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  conteiner:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'black'
   },
-});
+  box1:{
+    width:100,
+    height:100,
+    backgroundColor:"lightblue",
+    justifyContent:"center",
+    alignItems:'center',
+    marginBottom:20
+  }
+    
+
+})
+
